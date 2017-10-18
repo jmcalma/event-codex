f10cd77ff93ae9777e96aa16ec36d2d95cc89336
Jerahmeel Calma <jmcalma@cpp.edu>
2017-10-18 15:31:17 -0700

    Jerahmeel added a nodeunit test

--- client/public/scripts/testScripts.js
+++ client/public/scripts/testScripts.js
@@ -1,12 +1,20 @@
 //A6 used for A6, test add function.
 //Unit tester is called nodeunit found on https://github.com/caolan/nodeunit
 //install nodeunit globally via npm, run 'nodeunit 'scripts.js'
-function add(a, b) {
+function add(a, b) {//Alex's method
     return a + b;
 }
 
-exports.testAdd = function(test) {
+function mult(a, b) {//Jerahmeel's method
+	return a * b;
+}
+
+exports.testAdd = function(test) {//Alex's test
     test.equals(4, add(2,2));
     test.done();
 }
 
+exports.testMult = function(test) {//Jerahmeel's test
+	test.equals(55, mult(11,5));
+	test.done();
+}
