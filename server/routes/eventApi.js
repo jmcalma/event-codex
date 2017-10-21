49b900a0b9fcecf640f1cf6f2e9e4d2450b6cae0
Alex <ahkimea@cpp.edu>
2017-10-20 17:00:33 -0700

    added express instance to avoid error of 'Access-Control-Allow-Origin' due to browser accessing data from different ports

--- server/routes/eventApi.js
+++ server/routes/eventApi.js
@@ -1,6 +1,12 @@
 var events = require('./../dataBase/events');
 
 module.exports = app => {
+	app.use(function(req, res, next) {
+	  res.header("Access-Control-Allow-Origin", "*");
+	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	  next();
+	});
+
     app.get("/api/event", (req, res) => {
       res.send(events);
     });
