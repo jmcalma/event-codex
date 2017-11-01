2168612e75e2b301ed26b1aecdc7caa13b74b2bc
walkerzheng <9771330@qq.com>
2017-10-31 17:27:37 -0700

    add event model

---
+++ server/models/Event.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const eventSchema = new Schema({
+  host_title: String,
+  event_name: String,
+  location: String,
+  start_date: String,
+  start_time: String,
+  end_date: String,
+  end_time: String,
+  event_category: String,
+  event_description: String,
+  tags: String,
+  website_link: String,
+  subject: String
+});
+
+mongoose.model('events', eventSchema);
