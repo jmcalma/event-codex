f0fb81e3404e1a052f71bbfb6306fcf282b05e0b
walkerzheng <9771330@qq.com>
2017-10-31 17:13:50 -0700

    install mongoose library

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://walkerzheng:Zh19950513@ds123311.mlab.com:23311/event-codex');
 
 const app = express();
 app.use(bodyParser.json());
