af7c91dec3df168242ac1d75746009ba8a647930
walkerzheng <walkerhaozheng@gmail.com>
2017-11-04 21:12:45 -0700

    change event schema

--- server/routes/eventApi.js
+++ server/routes/eventApi.js
@@ -21,14 +21,27 @@ module.exports = app => {
 
 	app.post("/api/event", async (req, res) => {
 		const { email, title, location, start_date, start_time, end_date, end_time, website, description, tags } = req.body;
+		// var startDate = start_date.split("-");
+		// var year = startDate[0];
+		// var month = startDate[1];
+		// var day = startDate[2];
+		//
+		// console.log(year + ":" + month + ":" + day);
+		// var startTime = start_time.split(":");
+		// var hour = parseInt(startTime[0]);
+		// var second = startTime[1].substring(0, 2);
+		// var period = start_time.split(" ")[1];
+		// if (period == "pm") {
+		// 	hour += 12;
+		// }
+		var startDate = toDate(start_date, start_time);
+		console.log(startDate);
 		const event = new Event({
 		  host_email: email,
 		  event_name: title,
 		  location: location,
 		  start_date: Date.now(),
-		  start_time,
 		  end_date: Date.now(),
-		  end_time,
 		  event_category: "test",
 		  event_description: description,
 		  tags: tags,
@@ -43,3 +56,19 @@ module.exports = app => {
     }
 	});
 };
+
+function toDate(date, time) {
+	var dateArray = date.split("-");
+	var year = dateArray[0];
+	var month = dateArray[1];
+	var day = dateArray[2];
+
+	var timeArray = time.split(":");
+	var hour = parseInt(timeArray[0]);
+	var minute = timeArray[1].substring(0, 2);
+	if (time.split(" ")[1] == "pm") {
+		hour += 12;
+	}
+	console.log(year+ month + ":" + day + ":" + hour + ":" + minute);
+	return new Date(year + "-" + month + "-" + day +"T" + hour + ":" + minute + ":00Z");
+}
