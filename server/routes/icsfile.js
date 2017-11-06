81e006707556e1aeeb29c150a378348ea7c0fe63
walkerzheng <9771330@qq.com>
2017-11-06 13:41:59 -0800

    change download api name

--- server/routes/icsfile.js
+++ server/routes/icsfile.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = app => {
-    app.get("/icsfile/download", (req, res) => {
+    app.get("/download/icsfile/:id", (req, res) => {
       var file = __dirname + '/eventApi.js';
       res.download(file); // Set disposition and send it.
     });
