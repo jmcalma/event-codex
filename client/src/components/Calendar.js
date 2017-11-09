f0f8e52f69179a93381083b560e2c6015942d4e2
Alex <ahkimea@cpp.edu>
2017-11-08 17:47:02 -0800

    set default date to current date

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -1,21 +1,20 @@
 import React, { Component } from "react";
 import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
-import event from '../events';
+import events from '../events';
 
 let allViews = Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k])
 
 class Calendar extends Component {
   constructor() {
     super(); 
-
     BigCalendar.setLocalizer(
       BigCalendar.momentLocalizer(moment)
     );
-
     this.state = {
-      events: []
+      event: []
     };
+    //this.fetchEvents();
   };
 
    componentDidMount() {
@@ -23,11 +22,27 @@ class Calendar extends Component {
           .then((response) => {
               return response.json() })   
                   .then((json) => {
-                      this.setState({ events: json });
+                      this.setState({ event: json });
                   });
   };
 
+
+  fetchEvents = () => {
+      fetch('/api/event')
+      .then((resp) => resp.json())
+      .then(function(response) {
+        //console.log(response);
+        //console.log(events);
+        this.resp = response;
+        this.setState({ event: response });
+      })
+      .catch(function(error) {
+         console.log(error);
+      });
+  };
+
   render() {
+    console.log(this.state.event);
     return (
       <div id="calendar">
         <BigCalendar
@@ -35,7 +50,7 @@ class Calendar extends Component {
           popup
           {...this.props}
           views={{ month: true, week: false}}
-          events={this.state.events}
+          events={this.state.event}
           titleAccessor='location'
           startAccessor='start_date'
           endAccessor='end_date'
