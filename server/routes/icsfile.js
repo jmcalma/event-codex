1ec6b919feee79e6b0a3631d49a2e36fbd9380f8
walkerzheng <walkerhaozheng@gmail.com>
2017-11-09 23:33:34 -0800

    create first ics file

--- server/routes/icsfile.js
+++ server/routes/icsfile.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
+var fs = require('fs');
 
 module.exports = app => {
     app.get("/download/icsfile/:id", (req, res) => {
-      var file = __dirname + '/eventApi.js';
+      fs.appendFile('codex.ics', 'This is test file', function (err) {
+        if(err) throw err;
+      })
+      var file = __dirname + '/../codex.ics';
       res.download(file); // Set disposition and send it.
+
     });
 }
