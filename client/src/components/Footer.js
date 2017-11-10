6ad286f70efa8b572f9bb703d117ceb031b929e9
Alex <ahkimea@cpp.edu>
2017-11-10 10:48:02 -0800

    added footer page component

---
+++ client/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+	constructor() {
+		super();
+	}
+
+	render() {
+		return(
+			<div>
+				<footer id="footer">
+					<p> Random5 | CPP | Event Codex |  <a href="http://cs480-projects.github.io/teams-fall2017/random5/index.html">Website</a> </p>
+				</footer>
+			</div>
+		);
+	}
+}
+
+export default Footer;
