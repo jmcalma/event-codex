ed16d0f25d0c2cb65ac157cacc92edf2ac6e9e42
walkerzheng <walkerhaozheng@gmail.com>
2017-11-10 00:12:14 -0800

    use ics library to create ics file

--- server/routes/icsfile.js
+++ server/routes/icsfile.js
@@ -1,13 +1,36 @@
-const mongoose = require('mongoose');
-var fs = require('fs');
+const mongoose = require("mongoose");
+const fs = require("fs");
+const ics = require("ics");
 
 module.exports = app => {
-    app.get("/download/icsfile/:id", (req, res) => {
-      fs.appendFile('codex.ics', 'This is test file', function (err) {
-        if(err) throw err;
-      })
-      var file = __dirname + '/../codex.ics';
-      res.download(file); // Set disposition and send it.
+  app.get("/api/download/icsfile/:id", (req, res) => {
+    var fileName = __dirname + "/codex.ics";
 
-    });
-}
+    ics.createEvent(
+      {
+        title: "Dinner",
+        description: "Turkey is the best",
+        start: [2018, 1, 15, 6, 30],
+        duration: { minutes: 50 }
+      },
+      (error, value) => {
+        if (error) {
+          console.log(error);
+        }
+        // write file in server
+        fs.writeFileSync(fileName, value);
+        // user start to download the file from server
+        res.download(fileName, function(err) {
+          if (err) {
+            console.log(err);
+          } else {
+            // if user download success, then delet file in server
+            fs.unlink(fileName, function(err) {
+              if (err) throw err;
+            });
+          }
+        });
+      }
+    );
+  });
+};
