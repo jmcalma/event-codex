a7130fcb9d8e423fff43c68807a18919efc43720
Jerahmeel Calma <jmcalma@cpp.edu>
2017-11-10 17:39:55 -0800

    cleaned up /api/groups route

--- server/routes/meetupEventApi.js
+++ server/routes/meetupEventApi.js
@@ -4,25 +4,25 @@ var meetupEvents = [];
 
 module.exports = app => {
     app.get("/api/groups", async (req, res) => {
-			getTechGroups();
+      getGroupsFromMeetup();
 			res.send(groups);
     });
 
     app.get("/api/meetupEvents", async (req, res) => {
+      getGroupsFromMeetup();
 			getEventsFromMeetup();
 			res.send(meetupEvents);
     });
 }
 
-function getTechGroups() {
-	var link = "https://api.meetup.com/find/groups?photo-host=public&page=50&sig_id=240469031&category=34&only=urlname&sig=02bf65fd813cc9b9600a80d07aee3a43da37de51";
-  request(link, function (error, response, body) {
-  		if (!error && response.statusCode == 200) {
-     		var importedJSON = JSON.parse(body);
-     		groups = groups.concat(importedJSON);
-     		console.log(importedJSON);
-  		}
-	})
+function getGroupsFromMeetup() {
+    getCareerGroups();
+    getCarGroups();
+    getFoodGroups();
+    getMusicGroups(); 
+    getSocialGroups();
+    getSocialGroups();
+    getTechGroups();
 }
 
 function getCareerGroups() {
@@ -91,13 +91,24 @@ function getSportsGroups() {
   })
 }
 
+function getTechGroups() {
+  var link = "https://api.meetup.com/find/groups?photo-host=public&page=50&sig_id=240469031&category=34&only=urlname&sig=02bf65fd813cc9b9600a80d07aee3a43da37de51";
+  request(link, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var importedJSON = JSON.parse(body);
+        groups = groups.concat(importedJSON);
+        console.log(importedJSON);
+      }
+  })
+}
+
 function getEventsFromMeetup() {
-	var link = "https://api.meetup.com/HackerNestOC/events?photo-host=public&page=5&sig_id=240469031&only=name%2Cvenue%2Clocal_date%2Clocal_time%2Clink%2Cdescription&sig=d6fcf182f489d043b816b3ed4bfc76ad6ff0e962";
-	request(link, function (error, response, body) {
-  		if (!error && response.statusCode == 200) {
-     		var importedJSON = JSON.parse(body);
-     		meetupEvents = meetupEvents.concat(importedJSON);
-     		console.log(importedJSON);
-  		}
-	})
+  var link = "https://api.meetup.com/HackerNestOC/events?photo-host=public&page=5&sig_id=240469031&only=name%2Cvenue%2Clocal_date%2Clocal_time%2Clink%2Cdescription&sig=d6fcf182f489d043b816b3ed4bfc76ad6ff0e962";
+  request(link, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var importedJSON = JSON.parse(body);
+        meetupEvents = meetupEvents.concat(importedJSON);
+        console.log(importedJSON);
+      }
+  })
 }
