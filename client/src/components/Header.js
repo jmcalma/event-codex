65b23650f2c171e79529b434e75399a6bca26add
Alex <ahkimea@cpp.edu>
2017-11-11 13:13:01 -0800

    improved email and url regex validators

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -77,7 +77,8 @@ class Header extends Component {
 
   validateForm = () => {
     if(this.formIsValid()) {
-      this.addEvent();
+      //this.addEvent();
+      console.log('asdf');
     } else {
       alert("One or more of the fields is incomplete or has errors.");
     }
@@ -101,13 +102,16 @@ class Header extends Component {
         return false;
     }
 
-    if (!(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(contactEmail))) { 
+    var emailRegExValidate = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!(emailRegExValidate).test(contactEmail)) { 
        return false;
     }
 
+    //https://gist.github.com/dperini/729294
+    var urlRegExValidate = /^(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
     if(website === "") {
 
-    } else if (!(/^[a-zA-Z0-9]+\.[A-Za-z]+$/.test(website))) {
+    } else if (!(urlRegExValidate).test(website)) {
       return false;
     }
 
