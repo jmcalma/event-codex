84d948d1f42f533eeb1ed6dcb47d23bdfba0febe
winghunglau <winghl220@gmail.com>
2017-11-13 19:10:52 -0800

    clean up imports

--- server/index.js
+++ server/index.js
@@ -1,22 +1,23 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-require("./models/Event");
-mongoose.connect('mongodb://walkerzheng:Zh19950513@ds123311.mlab.com:23311/event-codex');
-
 const app = express();
+
+// Configurations
+const PORT = process.env.PORT || 5000;
+require("./models/Event");
+require("./routes/authRoutes")(app);
+require("./routes/eventApi")(app);
+require("./routes/icsfile")(app);
+require("./routes/meetupEventApi")(app);
+
+mongoose.connect('mongodb://walkerzheng:Zh19950513@ds123311.mlab.com:23311/event-codex');
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("server port 5000 : hello world");
 });
 
-require("./routes/authRoutes")(app);
-require("./routes/eventApi")(app);
-require("./routes/icsfile")(app);
-require("./routes/meetupEventApi")(app);
-
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("port 5000 is listen");
 });
