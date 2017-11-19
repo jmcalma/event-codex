612d15ed3bad10ed0dc1ea1b936576786f98ff55
Jerahmeel Calma <jmcalma@cpp.edu>
2017-11-18 17:14:03 -0800

    work in progress

--- server/routes/meetupEventApi.js
+++ server/routes/meetupEventApi.js
@@ -12,19 +12,43 @@ module.exports = app => {
     app.get("/api/meetupEvents", async (req, res) => {
       getGroupsFromMeetup();
       getEventsFromMeetup();
-      optimizeMeetupEvents();
+      //optimizeMeetupEvents();
       res.send(meetupEvents);
     });
+
+    // app.get("/api/event/title/:info", async (req, res) => {
+    //     var url = req.originalUrl;
+    //     var eventFilter = url.substring(url.lastIndexOf('/') + 1).trim();
+    //         filterByTitle(res, eventFilter);
+    // });
+
+    // app.get("/api/event/category/:info", async (req, res) => {
+    //     var url = req.originalUrl;
+    //     var eventFilter = url.substring(url.lastIndexOf('/') + 1).trim();
+    //         filterByCategory(res, eventFilter);
+    //     });
+
+    // app.get("/api/event/tag/:info", async (req, res) => {
+    //     var url = req.originalUrl;
+    //     var eventFilter = url.substring(url.lastIndexOf('/') + 1).trim();
+    //         filterByTag(res, eventFilter);
+    //     });
+    // app.get("/api/event/downloadics/:id", (req, res) => {
+    //   var url = req.originalUrl;
+    //   var eventId = url.substring(url.lastIndexOf('/') + 1).trim();
+    //   var event = _.where(meetupEvents, {meetupEvents.event_name: eventId})
+    //   enableDownload(res, event);
+    // });
 }
 
 function getGroupsFromMeetup() {
-    setTimeout(getCareerGroups(), 2000);
-    setTimeout(getCarGroups(), 2000);
-    setTimeout(getFoodGroups(), 2000);
-    setTimeout(getMusicGroups(), 2000); 
-    setTimeout(getSocialGroups(), 2000);
-    setTimeout(getSportsGroups(), 2000);
-    setTimeout(getTechGroups(), 2000);
+    setTimeout(getCareerGroups, 500);
+    setTimeout(getCarGroups, 500);
+    setTimeout(getFoodGroups, 500);
+    setTimeout(getMusicGroups, 500); 
+    setTimeout(getSocialGroups, 500);
+    setTimeout(getSportsGroups, 500);
+    setTimeout(getTechGroups, 500);
 }
 
 function getCareerGroups() {
@@ -98,27 +122,40 @@ function getTechGroups() {
 }
 
 function getEventsFromMeetup() {
-  var counter = 0;
-  var importedJSON;
-  var fullLink = "";
-  var linkHalf1 = "https://api.meetup.com/";
-  var linkHalf2 = "/events?&sign=true&photo-host=public&page=5&only=name,local_date,local_time,venue,group,link,description&key=d182f5649646f23517334541793f72";
-  for(var i in groups) {
-    fullLink = linkHalf1 + groups[i].urlname + linkHalf2;
-    setTimeout(request(fullLink, function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                  importedJSON = JSON.parse(body);
-                  meetupEvents = meetupEvents.concat(importedJSON);
-                  for(j = counter; j < meetupEvents.length; j++) {
-                    meetupEvents[j].group = groups[i].name;
+    var seconds = 0;
+    var counter = 0;
+    var importedJSON;
+    var fullLink = "";
+    var linkHalf1 = "https://api.meetup.com/";
+    var linkHalf2 = "/events?&sign=true&photo-host=public&page=1&only=name,local_date,local_time,venue,group,link,description&key=d182f5649646f23517334541793f72";
+    for(var i in groups) {
+        fullLink = linkHalf1 + groups[i].urlname + linkHalf2;
+        request(fullLink, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                importedJSON = JSON.parse(body);
+                meetupEvents = meetupEvents.concat(importedJSON);
+                if(counter == 30) {
+                    var stop = 15;
+                } else {
+                    var stop = 5;
+                }
+                for(j = counter; j < counter + stop; j++) {
                     meetupEvents[j].event_category = groups[i].category.name;
                     meetupEvents[j].tags = "" + meetupEvents[j].event_category + "," + groups[i].name;
                     counter++;
-                  }
                 }
-               }), 5000);
-  }
-  console.log(meetupEvents);
+                setTimeout(function() {
+                    if(counter % 10 == 0) {
+                        seconds = 5000;
+                    } else {
+                        seconds = 0;
+                    }
+                }, seconds);
+                console.log(counter + "  " + seconds);
+            }
+        });
+    }
+    console.log(meetupEvents);
 }
 
 function optimizeMeetupEvents() {
@@ -140,17 +177,73 @@ function optimizeMeetupEvents() {
     }
 }
 
-// function filterByCategory(res, eventFilter, index) {
-//     var filtered = _.where(meetupEvents, {groups[index].category: eventFilter});
+// function filterByCategory(res, eventFilter) {
+//     var filtered = _.where(meetupEvents, {meetupEvents.event_category: eventFilter});
+       // if (err) {
+       //      res.send('error');
+       //  }
 //     res.send(filtered);
 // }
 
-// function filterByTag(res, eventFilter, index) {
-//     var filtered = _.where(meetupEvents, {groups[index].tags: eventFilter});
+// function filterByTag(res, eventFilter) {
+//     var filtered = _.where(meetupEvents, {meetupEvents.tags: eventFilter});
+        // if (err) {
+        //     res.send('error');
+        // }
 //     res.send(filtered);
 // }
 
-// function filterByTitle(res, eventFilter, index) {
-//     var filtered = _.where(meetupEvents, {groups[index].name: eventFilter});
+// function filterByTitle(res, eventFilter) {
+//     var filtered = _.where(meetupEvents, {meetupEvents.event_name: eventFilter});
+       // if (err) {
+       //      res.send('error');
+       //  }
 //     res.send(filtered);
+// }
+// function enableDownload(res, event) {
+//     console.log("event: " + event);
+//     var title = event.event_name;
+//     var description = event.event_description;
+//     var startYear = event.start_date.getUTCFullYear();
+//     var startMonth = event.start_date.getUTCMonth() + 1;
+//     var startDate = event.start_date.getUTCDate();
+//     var startHour = event.start_date.getUTCHours();
+//     var startMin = event.start_date.getUTCMinutes();
+//     console.log("start: " + startYear + "-" + startMonth + "-" + startDate + "-" + startHour + "-" + startMin);
+//     var endYear = event.end_date.getUTCFullYear();
+//     var endMonth = event.end_date.getUTCMonth() + 1;
+//     var endDate = event.end_date.getUTCDate();
+//     var endHour = event.end_date.getUTCHours();
+//     var endMin = event.end_date.getUTCMinutes();
+//     console.log("end: " + endYear + "-" + endMonth + "-" + endDate + "-" + endHour + "-" + endMin);
+//     var location = event.location;
+//     var categories = [event.event_category];
+
+//     var fileName = __dirname + "/codex.ics";
+//     ics.createEvent(
+//       {
+//         title,
+//         description,
+//         start: [startYear, startMonth, startDate, startHour, startMin],
+//         duration: { days: endDate - startDate, hours: endHour - startHour, minutes: endMin - startMin },
+//         location,
+//         categories
+//       },
+//       (error, value) => {
+//         if (error) {
+//           throw new Exception('Create ics file fail');
+//         }
+//         // write file in server
+//         fs.writeFileSync(fileName, value);
+//         // user start to download the file from server
+//         res.download(fileName, function(err) {
+//           if (err) {
+//             console.log(err);
+//           } else {
+//             // if user download success, then delet file in server
+//             fs.unlink(fileName, function(err) { if (err) throw err; });
+//           }
+//         });
+//       }
+//     );
 // }
