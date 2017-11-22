5459a870fdb082c607c415feafcf571c95bbf41b
winghunglau <winghl220@gmail.com>
2017-11-21 22:53:11 -0800

    fix code format

--- server/index.js
+++ server/index.js
@@ -2,14 +2,13 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
+
 mongoose.Promise = global.Promise;
-mongoose.connect(
-  'mongodb://eventcodex:eventcodex@ds123311.mlab.com:23311/event-codex');
+mongoose.connect('mongodb://eventcodex:eventcodex@ds123311.mlab.com:23311/event-codex');
 
 app.use(bodyParser.json());
-
 app.get("/", (req, res) => {
-  res.send("server port 5000 : hello world");
+    res.send("server port 5000 : hello world");
 });
 
 // Configurations
@@ -19,7 +18,6 @@ require("./routes/authRoutes")(app);
 require("./routes/eventApi")(app);
 require("./routes/meetupEventApi")(app);
 
-
 app.listen(PORT, () => {
-  console.log("port 5000 is listen");
+    console.log("port 5000 is listen");
 });
--- server/routes/meetupEventApi.js
+++ server/routes/meetupEventApi.js
@@ -4,44 +4,40 @@ const fs = require("fs");
 const ics = require("ics");
 var groups = [];
 var meetupEvents = [];
-
 module.exports = app => {
     app.get("/api/groups", async (req, res) => {
         getGroupsFromMeetup();
         res.send(groups);
     });
-
     app.get("/api/meetupEvents", async (req, res) => {
         getGroupsFromMeetup();
         getEventsFromMeetup();
         res.send(meetupEvents);
     });
-
     app.get("/api/meetupEvents/title/:info", async (req, res) => {
         var url = req.originalUrl;
         var eventFilter = url.substring(url.lastIndexOf('/') + 1).trim();
         eventFilter = eventFilter.replace(/-/g, ' ');
-            filterByTitle(res, eventFilter);
+        filterByTitle(res, eventFilter);
     });
-
     app.get("/api/meetupEvents/category/:info", async (req, res) => {
         var url = req.originalUrl;
         var eventFilter = url.substring(url.lastIndexOf('/') + 1).trim();
         eventFilter = eventFilter.replace(/-/g, ' ');
-            filterByCategory(res, eventFilter);
-        });
-
+        filterByCategory(res, eventFilter);
+    });
     app.get("/api/meetupEvents/tag/:info", async (req, res) => {
         var url = req.originalUrl;
         var eventFilter = url.substring(url.lastIndexOf('/') + 1).trim();
         eventFilter = eventFilter.replace(/-/g, ' ');
-            filterByTag(res, eventFilter);
-        });
-
+        filterByTag(res, eventFilter);
+    });
     app.get("/api/meetupEvents/downloadics/:id", (req, res) => {
         var url = req.originalUrl;
         var eventId = url.substring(url.lastIndexOf('/') + 1).trim();
-        var event = _.findWhere(meetupEvents, {_id: eventId})
+        var event = _.findWhere(meetupEvents, {
+            _id: eventId
+        })
         enableDownload(res, event);
     });
 }
@@ -57,73 +53,73 @@ function getGroupsFromMeetup() {
 }
 
 function getCareerGroups() {
-  var link = "https://api.meetup.com/find/groups?photo-host=public&page=5&sig_id=240469031&category=2&only=name%2Curlname%2Ccategory.name&sig=bfcb6420e2cf880b9d61c94e5f12e0d67628395a";
-  request(link, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var importedJSON = JSON.parse(body);
-        groups = groups.concat(importedJSON);
-      }
-  })
+    var link = "https://api.meetup.com/find/groups?photo-host=public&page=5&sig_id=240469031&category=2&only=name%2Curlname%2Ccategory.name&sig=bfcb6420e2cf880b9d61c94e5f12e0d67628395a";
+    request(link, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var importedJSON = JSON.parse(body);
+            groups = groups.concat(importedJSON);
+        }
+    })
 }
 
 function getCarGroups() {
-  var link = "https://api.meetup.com/find/groups?photo-host=public&page=5&sig_id=240469031&category=3&only=name%2Curlname%2Ccategory.name&sig=c34867f7f368d17bb237dcc48af050000898cf5d";
-  request(link, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var importedJSON = JSON.parse(body);
-        groups = groups.concat(importedJSON);
-      }
-  })
+    var link = "https://api.meetup.com/find/groups?photo-host=public&page=5&sig_id=240469031&category=3&only=name%2Curlname%2Ccategory.name&sig=c34867f7f368d17bb237dcc48af050000898cf5d";
+    request(link, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var importedJSON = JSON.parse(body);
+            groups = groups.concat(importedJSON);
+        }
+    })
 }
 
 function getFoodGroups() {
-  var link = "https://api.meetup.com/find/groups?photo-host=public&page=5&sig_id=240469031&category=10&only=name%2Curlname%2Ccategory.name&sig=37a7bd2c3e2d917240d16c42725fe1e59badf1b2";
-  request(link, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var importedJSON = JSON.parse(body);
-        groups = groups.concat(importedJSON);
-      }
-  })
+    var link = "https://api.meetup.com/find/groups?photo-host=public&page=5&sig_id=240469031&category=10&only=name%2Curlname%2Ccategory.name&sig=37a7bd2c3e2d917240d16c42725fe1e59badf1b2";
+    request(link, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var importedJSON = JSON.parse(body);
+            groups = groups.concat(importedJSON);
+        }
+    })
 }
 
 function getMusicGroups() {
-  var link = "https://api.meetup.com/find/groups?photo-host=public&page=5&sig_id=240469031&category=21&only=name%2Curlname%2Ccategory.name&sig=7c42b049074997776eb741ba0f8d05978d17412b";
-  request(link, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var importedJSON = JSON.parse(body);
-        groups = groups.concat(importedJSON);
-      }
-  })
+    var link = "https://api.meetup.com/find/groups?photo-host=public&page=5&sig_id=240469031&category=21&only=name%2Curlname%2Ccategory.name&sig=7c42b049074997776eb741ba0f8d05978d17412b";
+    request(link, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var importedJSON = JSON.parse(body);
+            groups = groups.concat(importedJSON);
+        }
+    })
 }
 
 function getSocialGroups() {
-  var link = "https://api.meetup.com/find/groups?photo-host=public&page=5&sig_id=240469031&category=31&only=name%2Curlname%2Ccategory.name&sig=572b4e7aa0106b8611e5c21a84ff144cd144212a";
-  request(link, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var importedJSON = JSON.parse(body);
-        groups = groups.concat(importedJSON);
-      }
-  })
+    var link = "https://api.meetup.com/find/groups?photo-host=public&page=5&sig_id=240469031&category=31&only=name%2Curlname%2Ccategory.name&sig=572b4e7aa0106b8611e5c21a84ff144cd144212a";
+    request(link, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var importedJSON = JSON.parse(body);
+            groups = groups.concat(importedJSON);
+        }
+    })
 }
 
 function getSportsGroups() {
-  var link = "https://api.meetup.com/find/groups?photo-host=public&page=5&sig_id=240469031&category=32&only=name%2Curlname%2Ccategory.name&sig=3e6c75b2f395d79a61144e838f9b09ed31d4bd58";
-  request(link, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var importedJSON = JSON.parse(body);
-        groups = groups.concat(importedJSON);
-      }
-  })
+    var link = "https://api.meetup.com/find/groups?photo-host=public&page=5&sig_id=240469031&category=32&only=name%2Curlname%2Ccategory.name&sig=3e6c75b2f395d79a61144e838f9b09ed31d4bd58";
+    request(link, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var importedJSON = JSON.parse(body);
+            groups = groups.concat(importedJSON);
+        }
+    })
 }
 
 function getTechGroups() {
-  var link = "https://api.meetup.com/find/groups?photo-host=public&page=15&sig_id=240469031&category=34&only=name%2Curlname%2Ccategory.name&sig=b4b53e4756c2081109b9bd658c8e03ee918a13b7";
-  request(link, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var importedJSON = JSON.parse(body);
-        groups = groups.concat(importedJSON);
-      }
-  })
+    var link = "https://api.meetup.com/find/groups?photo-host=public&page=15&sig_id=240469031&category=34&only=name%2Curlname%2Ccategory.name&sig=b4b53e4756c2081109b9bd658c8e03ee918a13b7";
+    request(link, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var importedJSON = JSON.parse(body);
+            groups = groups.concat(importedJSON);
+        }
+    })
 }
 
 function getEventsFromMeetup() {
@@ -131,12 +127,12 @@ function getEventsFromMeetup() {
     var fullLink = "";
     var linkHalf1 = "https://api.meetup.com/";
     var linkHalf2 = "/events?&sign=true&photo-host=public&page=3&only=id,name,local_date,local_time,venue,group,link,description&key=d182f5649646f23517334541793f72";
-    for(let i = 0; i < 3; i++) {
+    for (let i = 0; i < 3; i++) {
         fullLink = linkHalf1 + groups[i].urlname + linkHalf2;
         console.log(groups[i].urlname);
         requestEvents(fullLink, importedJSON, i);
     }
-    for(let i = 4; i < 7; i++) {
+    for (let i = 4; i < 7; i++) {
         fullLink = linkHalf1 + groups[i].urlname + linkHalf2;
         console.log(groups[i].urlname);
         requestEvents(fullLink, importedJSON, i);
@@ -146,16 +142,16 @@ function getEventsFromMeetup() {
 
 function requestEvents(fullLink, importedJSON, i) {
     setTimeout(function() {
-        request(fullLink, function (error, response, body) {
+        request(fullLink, function(error, response, body) {
             if (!error && response.statusCode == 200) {
                 importedJSON = JSON.parse(body);
-                for(var j = 0; j < 3; j++) {
-                    if(importedJSON[j].hasOwnProperty('venue')) {
-                        importedJSON[j].location = "" + importedJSON[j].venue.address_1 + ", " + importedJSON[j].venue.city + ", " + importedJSON[j].venue.state + " " + importedJSON[j].venue.zip;   
+                for (var j = 0; j < 3; j++) {
+                    if (importedJSON[j].hasOwnProperty('venue')) {
+                        importedJSON[j].location = "" + importedJSON[j].venue.address_1 + ", " + importedJSON[j].venue.city + ", " + importedJSON[j].venue.state + " " + importedJSON[j].venue.zip;
                     } else {
                         importedJSON[j].location = "none";
                     }
-                    if(groups[i].category.name === "Tech") {
+                    if (groups[i].category.name === "Tech") {
                         importedJSON[j].event_category = "Technology";
                         importedJSON[j].tags = "" + groups[i].category.name + ", " + groups[i].name + ", " + importedJSON[j].location;
                     } else {
@@ -164,34 +160,34 @@ function requestEvents(fullLink, importedJSON, i) {
                     }
                     importedJSON[j].old = "oldData";
                 }
-                console.log("iteration: " + i + " seconds: " + (10000 + (1500*i)));//remove this later
+                console.log("iteration: " + i + " seconds: " + (10000 + (1500 * i))); //remove this later
                 meetupEvents = meetupEvents.concat(importedJSON);
             }
         });
-    }, (10000 + (1500*i)));
+    }, (10000 + (1500 * i)));
 }
 
 function optimizeMeetupEvents() {
-    for(var i in meetupEvents) {
-        if(typeof meetupEvents[i].id !== 'undefined') {
+    for (var i in meetupEvents) {
+        if (typeof meetupEvents[i].id !== 'undefined') {
             meetupEvents[i]._id = meetupEvents[i].id;
             delete meetupEvents[i].id;
         } else {
             meetupEvents[i]._id = "none";
         }
-        if(typeof meetupEvents[i].name !== 'undefined') {
+        if (typeof meetupEvents[i].name !== 'undefined') {
             meetupEvents[i].event_name = meetupEvents[i].name;
             delete meetupEvents[i].name;
         } else {
             meetupEvents[i].event_name = "none";
         }
-        if(typeof meetupEvents[i].venue !== 'undefined') {
+        if (typeof meetupEvents[i].venue !== 'undefined') {
             meetupEvents[i].location = "" + meetupEvents[i].venue.address_1 + ", " + meetupEvents[i].venue.city + ", " + meetupEvents[i].venue.state + " " + meetupEvents[i].venue.zip;
             delete meetupEvents[i].venue;
         } else {
             meetupEvents[i].location = "none";
         }
-        if(typeof meetupEvents[i].local_date !== 'undefined' && typeof meetupEvents[i].local_time !== 'undefined') {
+        if (typeof meetupEvents[i].local_date !== 'undefined' && typeof meetupEvents[i].local_time !== 'undefined') {
             var dateString = new Date(meetupEvents[i].local_date + "T" + meetupEvents[i].local_time);
             meetupEvents[i].start_date = dateString;
             meetupEvents[i].end_date = meetupEvents[i].start_date;
@@ -202,16 +198,16 @@ function optimizeMeetupEvents() {
             meetupEvents[i].start_date = dateString;
             meetupEvents[i].end_date = dateString;
         }
-        if(typeof meetupEvents[i].description !== 'undefined') {
+        if (typeof meetupEvents[i].description !== 'undefined') {
             meetupEvents[i].event_description = meetupEvents[i].description;
             delete meetupEvents[i].description;
         } else {
             meetupEvents[i].event_description = "none";
         }
-        if(typeof meetupEvents[i].link == 'undefined') {
+        if (typeof meetupEvents[i].link == 'undefined') {
             meetupEvents[i].link = "none";
         }
-        if(typeof meetupEvents[i].group !== 'undefined') {
+        if (typeof meetupEvents[i].group !== 'undefined') {
             meetupEvents[i].group_name = meetupEvents[i].group.name;
         } else {
             meetupEvents[i].group_name = "none";
@@ -221,37 +217,41 @@ function optimizeMeetupEvents() {
 }
 
 function filterByCategory(res, eventFilter) {
-    var filtered = _.where(meetupEvents, {category: eventFilter});
-    console.log("here are the events############################");//remove later
+    var filtered = _.where(meetupEvents, {
+        category: eventFilter
+    });
+    console.log("here are the events############################"); //remove later
     console.log(filtered);
-    console.log("###############################################");//remove later
+    console.log("###############################################"); //remove later
     res.send(filtered);
 }
 
 function filterValuePart(arr, part) {
     part = part.toLowerCase();
-
     return arr.filter(function(obj) {
-        return Object.keys(obj)
-                     .some(function(k) { 
-                               return obj[k].toLowerCase().indexOf(part) !== -1; 
-                           });
+        return Object.keys(obj).some(function(k) {
+            return obj[k].toLowerCase().indexOf(part) !== -1;
+        });
     });
 }
 
 function filterByTag(res, eventFilter) {
-    var filtered = _.where(meetupEvents, {tags: eventFilter});
-    console.log("here are the events############################");//remove later
+    var filtered = _.where(meetupEvents, {
+        tags: eventFilter
+    });
+    console.log("here are the events############################"); //remove later
     console.log(filtered);
-    console.log("###############################################");//remove later
+    console.log("###############################################"); //remove later
     res.send(filtered);
 }
 
 function filterByTitle(res, eventFilter) {
-    var filtered = _.where(meetupEvents, {event_name: eventFilter});
-    console.log("here are the events############################");//remove later
+    var filtered = _.where(meetupEvents, {
+        event_name: eventFilter
+    });
+    console.log("here are the events############################"); //remove later
     console.log(filtered);
-    console.log("###############################################");//remove later
+    console.log("###############################################"); //remove later
     res.send(filtered);
 }
 
@@ -270,29 +270,31 @@ function enableDownload(res, event) {
     var endMin = event.end_date.getUTCMinutes();
     var location = event.location;
     var categories = [event.event_category];
-
     var fileName = __dirname + "/codex.ics";
-    ics.createEvent(
-      {
+    ics.createEvent({
         title,
         description,
         start: [startYear, startMonth, startDate, startHour, startMin],
-        duration: { days: endDate - startDate, hours: endHour - startHour, minutes: endMin - startMin },
+        duration: {
+            days: endDate - startDate,
+            hours: endHour - startHour,
+            minutes: endMin - startMin
+        },
         location,
         categories
-      },
-      (error, value) => {
+    }, (error, value) => {
         if (error) {
-          throw new Exception('Create ics file fail');
+            throw new Exception('Create ics file fail');
         }
         fs.writeFileSync(fileName, value);
         res.download(fileName, function(err) {
-          if (err) {
-            console.log(err);
-          } else {
-            fs.unlink(fileName, function(err) { if (err) throw err; });
-          }
+            if (err) {
+                console.log(err);
+            } else {
+                fs.unlink(fileName, function(err) {
+                    if (err) throw err;
+                });
+            }
         });
-      }
-    );
+    });
 }
