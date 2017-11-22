9a033e57a1af8fad54af63d47df7aa3b2a5ab0d1
winghunglau <winghl220@gmail.com>
2017-11-21 22:47:08 -0800

    relocate google auth route

--- server/index.js
+++ server/index.js
@@ -2,14 +2,9 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
-const fs = require('fs');
-const readline = require('readline');
-const google = require('googleapis');
-const googleAuth = require('google-auth-library');
-const authorize = require('./google_calendar');
-
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://eventcodex:eventcodex@ds123311.mlab.com:23311/event-codex');
+mongoose.connect(
+  'mongodb://eventcodex:eventcodex@ds123311.mlab.com:23311/event-codex');
 
 app.use(bodyParser.json());
 
@@ -24,45 +19,6 @@ require("./routes/authRoutes")(app);
 require("./routes/eventApi")(app);
 require("./routes/meetupEventApi")(app);
 
-app.get("/google-calendar-auth", function (req, res) {
-	fs.readFile('client_secret.json', function processClientSecrets(err, content) {
-	  if (err) {
-	    console.log('Error loading client secret file: ' + err);
-	    return;
-	  }
-	 let events = [];
-	  authorize(JSON.parse(content), function(auth) {
-	  	function listEvents(auth) {
-		  var calendar = google.calendar('v3');
-		  calendar.events.list({
-		    auth: auth,
-		    calendarId: 'primary',
-		    timeMin: (new Date()).toISOString(),
-		    maxResults: 10,
-		    singleEvents: true,
-		    orderBy: 'startTime'
-		  }, function(err, response) {
-		    if (err) {
-		      console.log('The API returned an error: ' + err);
-		      return;
-		    }
-		    var events = response.items;
-		    if (events.length == 0) {
-		      console.log('No upcoming events found.');
-		    } else {
-		      console.log('Upcoming 10 events:');
-		      for (var i = 0; i < events.length; i++) {
-		        var event = events[i];
-		        var start = event.start.dateTime || event.start.date;
-		        events.add(`${start} - ${event.summary}`);
-		        console.log('%s - %s', start, event.summary);
-		      }
-		    }
-		  });
-		}
-	  });
-	});
-})
 
 app.listen(PORT, () => {
   console.log("port 5000 is listen");
