49c498e7e40ad932285aab0aae7e0d8f0bf5de11
winghunglau <winghl220@gmail.com>
2017-11-21 22:51:37 -0800

    implement events data extraction and response

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -67,5 +67,34 @@ module.exports = app => {
 };
 
 function callback(auth, res) {
-  res.send("todo show events");
+  var calendar = google.calendar('v3');
+  calendar.events.list({
+    auth: auth,
+    calendarId: 'primary',
+    timeMin: (new Date()).toISOString(),
+    maxResults: 10,
+    singleEvents: true,
+    orderBy: 'startTime'
+  }, function(err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      res.send('The API returned an error: ' + err);
+      return;
+    }
+    var events = response.items;
+    if (events.length == 0) {
+      console.log('No upcoming events found.');
+      res.send('No upcoming events found.');
+    } else {
+      var list = [];
+      console.log('Upcoming 10 events:');
+      for (var i = 0; i < events.length; i++) {
+        var event = events[i];
+        var start = event.start.dateTime || event.start.date;
+        console.log('%s - %s', start, event.summary);
+        list.push(`${start} - ${event.summary}`);
+      }
+      res.send(list);
+    }
+  });
 }
