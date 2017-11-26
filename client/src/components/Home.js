c738dc971f9b14387d8a9610cc0866ed84a31a84
itpwang <itpwang@gmail.com>
2017-11-25 23:43:27 -0800

    Updated homepage styling of boxes.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,11 +2,19 @@ import React from "react";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {blue500} from 'material-ui/styles/colors';
 
+
 class Home extends React.Component{
     render(){
-        const clickables=[
+        const clickables={
+            textAlign: 'center',
+            display : 'inline-block',
+            width: '25%',
+            position: 'relative',
+            border: '1px solid black',
+            // padding: '3% 3% 3% 3%',
+            margin: '4% 4% 4% 4%',
 
-        ];
+        };
 
         // quick export button styling - TODO: move to seperate styling file, use flowbox
         const qExport = {
@@ -24,13 +32,13 @@ class Home extends React.Component{
             <div>
                 <MuiThemeProvider>
                 <h2> Most Popular: </h2>
-                    <div style={qExport} className = "quick-export" onClick={this.exportCalendar}>
+                    <div style={clickables} className = "quick-export" onClick={this.exportCalendar}>
                         <h3> box1 </h3>
                     </div>
-                    <div style={qExport} className = "quick-export">
+                    <div style={clickables} className = "quick-export">
                         <h3>box2</h3>
                     </div>
-                    <div style={qExport} className = "quick-export">
+                    <div style={clickables} className = "quick-export">
                         <h3>box3</h3>
                     </div>
 
