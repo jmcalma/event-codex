269453dfe6cd0b0b5fb9b425076c67597ebd33a5
itpwang <itpwang@gmail.com>
2017-12-06 13:05:46 -0800

    Add test functions

--- client/public/scripts/testScripts.js
+++ client/public/scripts/testScripts.js
@@ -13,6 +13,10 @@ function subtract(a, b) {//Wing Hung's method
 	return a - b;
 }
 
+function divide(a, b) {//Ivan's method
+	return a / b;
+}
+
 exports.testAdd = function(test) {//Alex's test
     test.equals(4, add(2,2));
     test.done();
@@ -25,5 +29,10 @@ exports.testMult = function(test) {//Jerahmeel's test
 
 exports.testSubtract = function(test) {//Wing Hung's test
 	test.equals(10, subtract(25, 15));
+	test.done();
+}
+
+exports.testDivide = function(test) {//Ivan's test
+	test.equals(3, divide(15,5));
 	test.done();
 }
